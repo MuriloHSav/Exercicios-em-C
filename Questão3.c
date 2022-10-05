ee25fdbd0dcaaf44c386eeb24a75f758441f3363
Murilo Henrique Gomes Fernandes <114261608+MuriloHSav@users.noreply.github.com>
2022-10-05 20:34:11 -0300

    Conversão Decimal - Octal e Decimal - Hexadecimal

---
+++ Questão3.c
@@ -0,0 +1,21 @@
+/*Escreva um programa que leia um número na base decimal e em seguida imprima esse
+mesmo número nas bases octal e hexadecimal.*/
+
+#include <stdio.h>
+
+int main(){
+
+	int decimal;
+	
+	//Entrada do valor
+	
+	printf("Insira um valor decimal: ");
+	scanf("%d", &decimal);
+	
+	//Conversão e Apresentação do valor
+	
+	printf("O valor recebido foi: %d\n", decimal);
+	printf("O valor convertido para octal e: %o\n", decimal);
+	printf("O valor convertido para hexadecimal e: %X", decimal);
+	
+}
