a90dc38b5337f3d75fa0e9c7327868c656c4912c
Murilo Henrique Gomes Fernandes <114261608+MuriloHSav@users.noreply.github.com>
2022-10-06 21:41:52 -0300

    Números pares entre 1 e 50

---
+++ Questão15.c
@@ -0,0 +1,16 @@
+/*Faça um programa que mostre todos os números pares existentes entre 1 e 50.*/
+
+#include <stdio.h>
+
+int main(){
+	
+	int par;
+	
+	//Condicional para filtrar pares e ímpares
+	
+	for(par = 1; par < 51; par++){
+		if(par % 2 == 0){
+			printf("%d - ", par);
+		}
+	}
+}
